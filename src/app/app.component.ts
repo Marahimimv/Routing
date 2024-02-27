340e3ce4d614472d1155b109d671f9be0f8322fa
Faiqmzade3 <faiq.memmedzade3@gmail.com>
2024-02-27 19:24:48 +0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, FormsModule, RouterLink],
+  template: `
+  <h1>Routing</h1>
+  <a href="/c1">C1 Component</a> ||
+  <a routerLink="/c2">C2 Component</a> ||
+  <a routerLink="/c3">C3 Component</a>
+  <router-outlet></router-outlet>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Routing';
+
 }
